705d8bdd24a2b3b8334dd57c8fe809761ebf4fb2
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-24 13:18:54 -0700

    made user madel

---
+++ Auth/Server/db/models/user-model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = new mongoose.Schema({
+    email:{
+        type:String,
+        required: true,
+        minlength:5,
+        trim: true,
+        unique: true,
+        validate:{
+            validator: validator.isEmail,
+            message:"{VALUE} is not an email"
+        
+        }
+    },
+    password:{
+        type:String,
+        required: true,
+        trim: true
+    },
+    username:{
+        type:String,
+        required: true,
+        minlength: 8,
+        trim: true,
+        unique: true
+    }
+});
+
+module.exports = mongoose.model('User', User);
