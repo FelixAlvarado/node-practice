fa673ae23e8bd87891bf7c73fff666bada703057
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-24 16:16:58 -0700

    completed login

--- Auth/Server/routes/user-routes.js
+++ Auth/Server/routes/user-routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../db/models/user-model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/register', (req,res) => {
     const {email,password,username} = req.body;
@@ -18,6 +20,29 @@ router.post('/register', (req,res) => {
     }).catch(err =>{
         if(err){
             return res.status(400).send({error:err});
+        }
+        return res.status(400).send();
+    });
+});
+
+router.post('/login', (req,res) => {
+    const {username,password} = req.body;
+    User.findOne({username}).then((user) => {
+        if(!user){
+            return res.status(400).send();
+        }
+        bcrypt.compare(password, user.password).then(match => {
+            if(!match){
+                return res.status(401).send();
+            }
+            let token = jwt.sign({_id:user._id}, 'secret');
+            return res.status(201).send({token});
+        }).catch(err => {
+            return res.status(401).send({error:err});
+        });
+    }).catch((err) => {
+        if(err) {
+            return res.status(400).send(err);
         }
         return res.status(400).send();
     });
