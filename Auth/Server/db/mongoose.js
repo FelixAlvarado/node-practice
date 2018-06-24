3ae9940ac33d2de4bc5c0f1f6f9f83d7ca3e90b8
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-24 00:18:33 -0700

    set up mongodb database

--- Auth/Server/db/mongoose.js
+++ Auth/Server/db/mongoose.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('')
-.then(() => console.log('DB connected'))
-.catch(err => console.log(err));
+mongoose.connect('mongodb://authUser:a123456@ds217131.mlab.com:17131/auth')
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log(err));
 
 module.exports = mongoose;
--- Auth/Server/server.js
+++ Auth/Server/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('./db/mongoose');
 
-app.listen(3000);
+app.get('/', (req,res) => {
+    res.send('welcome');
+});
+
+const PORT = 3000;
+
+app.listen(3000, () => {
+    console.log(`server started on port ${PORT}`);
+});
